138e7c6e489641cbae864578b3e08813cbe2d4a8
Chris Aronchick (COMPUCOM SYSTEMS INC) <v-chraro@microsoft.com>
2019-04-11 11:09:05 -0700

    Updated packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,48 @@
+// module.exports = {
+//   "parser": 'babel-eslint',
+//   "env": {
+//     "browser": true,
+//     "commonjs": true,
+//     "es6": true,
+//     "node": true
+//   },
+//   "extends": ["eslint:recommended", "plugin:react/recommended"],
+//   "parserOptions": {
+//     "ecmaFeatures": {
+//       "experimentalObjectRestSpread": true,
+//       "jsx": true
+//     },
+//     "sourceType": "module"
+//   },
+//   "plugins": [ "react" ],
+//   "rules": {
+//     "indent": ["error", 2],
+//     "linebreak-style": ["error","unix"],
+//     "quotes": ["error","single"],
+//     "semi": ["error","always"],
+//     "no-console": ["warn", { "allow": ["info", "error"] }]
+//   }
+// };
 module.exports = {
-  "parser": 'babel-eslint',
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended"],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "sourceType": "module"
+    sourceType: 'module',
   },
-  "plugins": [ "react" ],
-  "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error","unix"],
-    "quotes": ["error","single"],
-    "semi": ["error","always"],
-    "no-console": ["warn", { "allow": ["info", "error"] }]
-  }
+  plugins: ['react'],
+  rules: {
+    // You can do your customizations here...
+    // For example, if you don't want to use the prop-types package,
+    // you can turn off that recommended rule with: 'react/prop-types': ['off']
+  },
 };
