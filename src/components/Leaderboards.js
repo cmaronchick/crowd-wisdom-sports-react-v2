0d60c37ae0becf94f3707fef61638c1fa4ab14a1
Chris Aronchick (COMPUCOM SYSTEMS INC) <v-chraro@microsoft.com>
2019-05-16 09:10:49 -0700

    Initial leaderboard commit

--- src/components/Leaderboards.js
+++ src/components/Leaderboards.js
@@ -13,12 +13,15 @@ export default class Leaderboard extends Component {
     async componentDidMount() {
         const userSession = await Auth.currentSession()
         let leaderboardData = await apis.fetchOverallLeaderboard(userSession, 'nfl', 2018, 'post', 21)
+        this.setState({leaderboardData})
         console.log('leaderboardData: ', leaderboardData)
     }
     render() {
         return (
             <div>
-                
+                {this.state.leaderboardData ? (
+                    <div>LeaderboardData received</div>
+                ) : null}
             </div>
         )
     }
