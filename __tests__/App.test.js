95dcf45d88ea871aeaf49b94202ef36407c05e98
Chris Aronchick <v-chraro@microsoft.com>
2019-10-23 13:35:26 -0700

    Fixed App.test and CrowdCompare Test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from '../src/components/App'
 import renderer from 'react-test-renderer'
 import Auth from '@aws-amplify/auth'
 
-test(" null Auth test", () => {
-    const user = Auth.currentAuthenticatedUser()
-    .then(userObj => {
-        console.log('userObj: ', userObj)
-        expect(userObj).toBe('not authenticated')
-    })
-    .catch(userObjReject => {
+test(" null Auth test", async () => {
+    try {
+    const user = await Auth.currentAuthenticatedUser()
+    
+        console.log('userObj: ', user)
+        expect(user).toBe('not authenticated')
+    } catch(userObjReject) {
         console.log('userObjReject: ', userObjReject)
         
         expect(userObjReject).toBe('not authenticated')
-    })
+    }
 })
 
 test("snapshot test", () => {
     const tree = renderer
-        .create(<App />)
+        .create(<Router><App /></Router>)
         .toJSON()
     expect(tree).toMatchSnapshot();
 })
