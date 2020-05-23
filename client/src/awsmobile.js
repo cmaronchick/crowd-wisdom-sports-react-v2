775a112de99e80cfa7bee4aa87f3f96567423e5c
Chris Aronchick <chrisaronchick@gmail.com>
2020-05-22 17:05:06 -0700

    Adding missing files

---
+++ client/src/awsmobile.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+    "aws_project_region": "us-west-2",
+    "aws_cognito_region": "us-west-2",
+    "aws_user_pools_id": "us-west-2_zym3aCbQ3",
+    "aws_user_pools_web_client_id": "2n15lhk845sucm0k4fejjqcbev"
+};
+
+
+export default awsmobile;
