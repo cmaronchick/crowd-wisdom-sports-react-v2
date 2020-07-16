0b24a518ca6e73af6d442c9d023737c678790f3e
Chris Aronchick <chrisaronchick@gmail.com>
2020-07-15 22:33:08 -0700

    Fixed webpack less config

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,37 @@ module.exports = {
           }
         },
       },
+      {
+        test: /\.less$/, // match any .less file, 
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          { loader: "less-loader",
+            options: {
+              lessOptions: {
+                modifyVars: { 
+                  '@primary-color': '#0a1f8f',
+                  '@secondary-color': '#124734',
+                  '@tertiary-color': '#e04403',
+                  '@info-color': '@primary-color',
+                  '@success-color': '#124734',
+                  '@processing-color': '@primary-color',
+                  '@error-color': 'rgba(224, 20, 7, 1.0)',
+                  '@highlight-color': '#f6dfa4',
+                  '@warning-color': 'rgba(224, 20, 7, 1.0)',
+                  '@normal-color': '#231f20',
+                  '@white': '#fff',
+                  '@black': '#231f20',
+                  '@layout-header-background': '#231f20',
+                  '@layout-header-color': '#f6dfa4'
+                  },
+                  javascriptEnabled: true
+              },
+              sourceMap: true
+            }
+          }
+        ]
+      },
       
       {
         test: /\.(s*)css$/, // match any .scss or .css file, 
