5a9cea9f5161de51272f15dd8c18e1fed1234b76
Chris Aronchick <chrisaronchick@gmail.com>
2021-09-07 12:32:37 -0700

    Updating to be proper component

--- client/src/components/static/Privacy.js
+++ client/src/components/static/Privacy.js
@@ -1,4 +1,11 @@
-<p>This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
+
+import React from 'react'
+import '../../styles/Rules.less'
+
+const Privacy = () => {
+    return (
+     <div>
+     <p>This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
 <p>
 What personal information do we collect from the people that visit our blog, website or app?
 </p>
@@ -172,3 +179,6 @@ USA<br/>
 <p>
 Last Edited on 2018-04-15
 </p>
+</div>)
+ 
+ export default Privacy
