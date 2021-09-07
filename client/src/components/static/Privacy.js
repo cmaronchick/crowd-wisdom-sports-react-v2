1c22c43a3665f33f436fcbc277bfbdc0666f9ce0
Chris Aronchick <chrisaronchick@gmail.com>
2021-09-07 13:03:14 -0700

    Last style update

--- client/src/components/static/Privacy.js
+++ client/src/components/static/Privacy.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../../styles/Rules.less'
 
 const Privacy = () => (
-     <div style={{textAlign: 'left'}}>
+     <div style={{textAlign: 'left', padding: 10}}>
      <p>This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
 <h1>
 What personal information do we collect from the people that visit our blog, website or app?
