1dbe5e066f3df83b271aeb07c4e171a26bf15c1a
Chris Aronchick <chrisaronchick@gmail.com>
2021-11-15 21:18:44 -0800

    Added view to other user predictions

--- client/src/components/profile/Predictions.js
+++ client/src/components/profile/Predictions.js
@@ -2,8 +2,9 @@ import React, { useEffect} from 'react'
 import { connect } from 'react-redux'
 import { getUserPredictions } from '../../redux/actions/predictionsActions'
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min'
+import StakeIcon from '../../images/stake-image-blue-dual-ring.svg'
 
-import { Table, Typography } from 'antd'
+import { Table, Typography, Spinner } from 'antd'
 
 const { Title } = Typography
 
@@ -30,17 +31,20 @@ export const Predictions = (props) => {
             dataIndex: 'awayTeam',
             key: 'gameId',
             render: (awayTeam) => {
-                return (<div>{awayTeam.code}<br />{awayTeam.score}</div>)
+                console.log(`awayTeam`, awayTeam)
+                return (<div>{awayTeam.code ? (<span>{awayTeam.code}<br /></span>) : null}{awayTeam.score}</div>)
             },
-            fixed: 'left'
+            fixed: 'left',
+            width: 120
         },
         {
             title: 'Home',
             dataIndex: 'homeTeam',
             render: (homeTeam) => {
-                return (<div>{homeTeam.code}<br/>{homeTeam.score}</div>)
+                return (<div>{homeTeam.code ? (<span>{homeTeam.code}<br /></span>) : null}{homeTeam.score}</div>)
             },
-            fixed: 'left'
+            fixed: 'left',
+            width: 120
         },
         {
             title: 'Spread',
@@ -48,7 +52,8 @@ export const Predictions = (props) => {
             render: (odds) => {
                 return (<div>{odds.spread}</div>)
             },
-            fixed: 'left'
+            fixed: 'left',
+            width: 120
         },
         {
             title: 'Total',
@@ -56,20 +61,70 @@ export const Predictions = (props) => {
             render: (odds) => {
                 return (<div>{odds.total}</div>)
             },
-            fixed: 'left'
+            fixed: 'left',
+            width: 120
         },
         {
             title: 'Prediction Score',
             dataIndex: 'predictionScore',
-            width: 100
+            render: (predictionScore) => {
+                return (<div>{predictionScore ? predictionScore : ''}</div>)
+            },
+            width: 120
         }
 
     ]
     return (
         <div>
             <Title>{userId} Predictions</Title>
-            {comparedPredictions && comparedPredictions.length > 0 ? (
-                <Table className="comparisonTable" rowKey="gameId" dataSource={comparedPredictions} columns={columns} />
+            {props.predictions.comparedUser?.gettingPredictions ? (
+                        <div className="oddsRow" style={{backgroundColor: '#fff', padding: 20}}>
+                            <img src={StakeIcon} className="loadingIcon" alt="Odds Loading" />
+                        </div>
+                ) : comparedPredictions && comparedPredictions.length > 0 ? (
+                <Table className="comparisonTable" rowKey="gameId" dataSource={comparedPredictions} columns={columns}
+                
+                expandable={{
+                    expandedRowRender: record => {
+                        const game = props.games.games[record.gameId]
+                        console.log(`game`, game)
+                        const gameResult = game ? { odds: game.odds, awayTeam: { score: game.results.awayTeam.score }, homeTeam: { score: game.results.homeTeam.score }} : null
+                        const myPrediction = props.predictions.user[record.gameId]
+                        const myColumns = [
+                            {
+                                title: 'Me',
+                                dataIndex: 'odds',
+                                className: 'embeddedTableNameColumn',
+                                render: (odds) => {
+                                    return (<div>Me</div>)
+                                },
+                                fixed: 'left'
+                            },
+                            ...columns]
+                        const gameColumns = [
+                            {
+                                title: 'FINAL',
+                                dataIndex: 'odds',
+                                className: 'embeddedTableNameColumn',
+                                render: (odds) => {
+                                    return (<div>FINAL</div>)
+                                },
+                                fixed: 'left'
+                            },
+                            ...columns]
+                        return (
+                            <div>
+                                {myPrediction && (
+                                    <Table showHeader={false} dataSource={[myPrediction]} columns={myColumns} pagination={false} />
+                                )}
+                                {gameResult && (
+                                    <Table showHeader={false} dataSource={[gameResult]} columns={gameColumns} pagination={false} />
+                                )}
+                            {/* <div style={{ margin: 0 }}>FINAL: {game.results.awayTeam.score}-{game.results.homeTeam.score} Spread: {game.odds.spread} Total: {game.odds.total} </div> */}
+                            </div>)
+                    },
+                    columnWidth: 110
+                }} />
             ) : (
                 <div>No Predictions available yet. Check back soon!</div>
             )}
@@ -79,7 +134,8 @@ export const Predictions = (props) => {
 
 const mapStateToProps = (state) => ({
     sport: state.sport,
-    predictions: state.predictions
+    predictions: state.predictions,
+    games: state.games
 })
 
 const mapDispatchToProps = {
