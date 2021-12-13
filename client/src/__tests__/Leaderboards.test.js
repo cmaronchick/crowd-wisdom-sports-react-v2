7760fe072a0519f30dd79f745ed1635021d5d7d3
Chris Aronchick <chrisaronchick@gmail.com>
2021-12-12 21:20:56 -0800

    Got tests working

--- client/src/__tests__/Leaderboards.test.js
+++ client/src/__tests__/Leaderboards.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { render } from '@testing-library/react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Leaderboards from '../components/leaderboards/Leaderboards'
 import { Provider } from 'react-redux'
@@ -15,17 +14,31 @@ import {
   } from '@testing-library/dom'
   // adds special assertions like toHaveTextContent
   import '@testing-library/jest-dom/extend-expect'
+import { SET_USER } from '../redux/types';
+import { 
+  screen, render, getByAltText } from '@testing-library/react'
 
 describe('render the game screen', () => {
-    
+    test('show login UI when user is not authenticated', () => {
+        const {getByText} = render(
+            <Provider store={store}>
+                <Router><Route component={Leaderboards} /></Router>
+            </Provider>);
+            console.log({getByText});
+            const NoLeaderboard = getByText('Login/Sign Up');
+            expect(NoLeaderboard).toBeInTheDocument();
+    })
     test('present a loading game message when game is missing', () => {
+        store.dispatch({
+            type: SET_USER
+        })
         const {getByText} = render(
         <Provider store={store}>
             <Router><Route component={Leaderboards} /></Router>
         </Provider>);
         console.log({getByText});
         const NoLeaderboard = getByText('No Weekly Leaderboard');
-        expect(NoLeaderboard).toBeInTheDocument();;
+        expect(NoLeaderboard).toBeInTheDocument();
     })
 
 })
