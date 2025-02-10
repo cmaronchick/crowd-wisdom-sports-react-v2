828fd4f3260a9428e9ea51071a8236bdc636fefd
Chris Aronchick <chrisaronchick@gmail.com>
2025-02-10 10:46:46 -0800

    Fixed server tests

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,7 @@ module.exports = {
         }
       }]
     ],
-    "plugins": [["@babel/plugin-proposal-class-properties"],
+    "plugins": [["@babel/plugin-transform-class-properties"],
         ["@babel/plugin-transform-runtime", {
         "regenerator": true
       }]
